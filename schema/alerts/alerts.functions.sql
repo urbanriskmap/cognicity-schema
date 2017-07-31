821fc744ae5d0645ae95654ba738ef92bf642940
Tomas <tomasholderness@gmail.com>
2017-07-31 15:11:03 -0400

    prototype alerts schema

--- schema/alerts/alerts.functions.sql
+++ schema/alerts/alerts.functions.sql
@@ -1,16 +1,61 @@
--- Computes alerts from existing report locations when new report received
-CREATE OR REPLACE FUNCTION alerts.compute() RETURNS trigger AS $$
+-- Function: alerts.compute()
+
+-- DROP FUNCTION alerts.compute();
+
+CREATE OR REPLACE FUNCTION alerts.compute()
+  RETURNS trigger AS
+$BODY$
 DECLARE
 alert RECORD;
 BEGIN
-  FOR alert IN EXECUTE 'SELECT NEW.pkey, NEW.report_data, NEW.created_at,  d.card_id, d.username, d.network, d.language, ST_DISTANCE(ST_TRANSFORM(NEW.the_geom, 32748), ST_TRANSFORM(b.the_geom, 32748)) FROM cognicity.all_reports b, grasp.reports c, grasp.cards d WHERE ST_DWITHIN(ST_TRANSFORM(NEW.the_geom, 32748), ST_TRANSFORM(b.the_geom, 32748), 1000) AND 22005 <> b.pkey AND b.source = "grasp" AND b.fkey = c.pkey AND d.card_id = c.card_id' LOOP
+  --SELECT into report_id, report_area c.PKEY, c.tags->>'instance_region_code' FROM cognicity.all_reports c, grasp.reports g WHERE c.fkey = g.pkey AND c.source = 'grasp' AND g.card_id = NEW.card_id;
 
-    PERFORM pg_notify('watchers', alert.report_data)
+  --PERFORM pg_notify('watchers', '{"' || TG_TABLE_NAME || '":{"pkey":"' || NEW.pkey || '", "username": "'|| NEW.username ||'", "network": "' || NEW.network || '", "language": "'|| NEW.language ||'", "report_id": "' || report_id ||'", "report_impl_area": "' || report_area || '"}}' );
+  --RETURN new;
 
+  FOR alert IN SELECT d.card_id, d.username, d.network, d.language, ST_DISTANCE(ST_TRANSFORM(NEW.the_geom, 32748), ST_TRANSFORM(b.the_geom, 32748)) FROM cognicity.all_reports b, grasp.reports c, grasp.cards d WHERE ST_DWITHIN(ST_TRANSFORM(NEW.the_geom, 32748), ST_TRANSFORM(b.the_geom, 32748), 1000) AND 22005 <> b.pkey AND b.source = 'grasp' AND b.fkey = c.pkey AND d.card_id = c.card_id LOOP
+
+  -- this should be swapped out for query on alerts.locations  WHERE alerts.users.subscribed === TRUE
+
+    PERFORM pg_notify('alerts', alert::text);
+
+ END LOOP;
+ RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION alerts.compute()
+  OWNER TO postgres;
 
+-- Trigger an alert
 CREATE TRIGGER watch_reports_alerts_trigger
   AFTER UPDATE ON cognicity.all_reports
   FOR EACH ROW
   EXECUTE PROCEDURE alerts.compute();
+
+-- Subscribe a GRASP user to alerts service
+CREATE OR REPLACE FUNCTION alerts.register()
+  RETURNS trigger AS
+    $BODY$
+      DECLARE
+      --something
+      userkey BIGINT;
+      BEGIN
+
+      INSERT INTO alerts.users (username, network, subscribed) SELECT c.username, c.network, c.language FROM grasp.cards c WHERE c.card_id = NEW.card_id ON CONFLICT c.username DO NOTHING RETURNING pkey INTO userkey;
+
+      INSERT INTO alerts.locations (userkey, location) VALUES (userkey, NEW.the_geom);
+
+      RETURN NEW;
+    END;
+    $BODY$
+      LANGUAGE plpgsql VOLATILE
+      COST 100;
+    ALTER FUNCTION alerts.register()
+      OWNER TO postgres;
+
+CREATE TRIGGER register_grasp_users_alerts_trigger
+  BEFORE INSERT ON grasp.reports
+  FOR EACH ROW
+  EXECUTE PROCEDURE alerts.register();
--- schema/alerts/alerts.schema.sql
+++ schema/alerts/alerts.schema.sql
@@ -1,1 +1,21 @@
+-- Schema for alerting support
 CREATE schema alerts;
+
+-- Table for users
+CREATE TABLE alerts.users (
+  pkey BIGSERIAL PRIMARY KEY,
+  username VARCHAR UNIQUE,
+  network VARCHAR,
+  language VARCHAR,
+  subscribed BOOLEAN
+);
+
+-- Table for user alerting locations
+CREATE TABLE alerts.locations (
+  pkey BIGSERIAL PRIMARY KEY,
+  userkey BIGINT REFERENCES alerts.users(pkey),
+  location GEOMETRY(Point,4326)
+);
+
+-- Add a GIST spatial index
+CREATE INDEX gix_alert_locations ON alerts.locations USING gist (the_geom);
