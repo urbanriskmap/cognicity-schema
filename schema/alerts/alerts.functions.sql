bf599077e2710b12006c64bd3d70b7d2c343e088
Tomas <tomasholderness@gmail.com>
2017-08-04 14:51:57 -0400

    working alert schema

--- schema/alerts/alerts.functions.sql
+++ schema/alerts/alerts.functions.sql
@@ -1,24 +1,15 @@
 -- Function: alerts.compute()
-
--- DROP FUNCTION alerts.compute();
-
+-- Determine whether alert locations are within radius of new report
 CREATE OR REPLACE FUNCTION alerts.compute()
   RETURNS trigger AS
 $BODY$
 DECLARE
-alert RECORD;
+alert RECORD; -- store alert location
 BEGIN
-  --SELECT into report_id, report_area c.PKEY, c.tags->>'instance_region_code' FROM cognicity.all_reports c, grasp.reports g WHERE c.fkey = g.pkey AND c.source = 'grasp' AND g.card_id = NEW.card_id;
-
-  --PERFORM pg_notify('watchers', '{"' || TG_TABLE_NAME || '":{"pkey":"' || NEW.pkey || '", "username": "'|| NEW.username ||'", "network": "' || NEW.network || '", "language": "'|| NEW.language ||'", "report_id": "' || report_id ||'", "report_impl_area": "' || report_area || '"}}' );
-  --RETURN new;
-
-  FOR alert IN SELECT d.card_id, d.username, d.network, d.language, ST_DISTANCE(ST_TRANSFORM(NEW.the_geom, 32748), ST_TRANSFORM(b.the_geom, 32748)) FROM cognicity.all_reports b, grasp.reports c, grasp.cards d WHERE ST_DWITHIN(ST_TRANSFORM(NEW.the_geom, 32748), ST_TRANSFORM(b.the_geom, 32748), 1000) AND 22005 <> b.pkey AND b.source = 'grasp' AND b.fkey = c.pkey AND d.card_id = c.card_id LOOP
-
-  -- this should be swapped out for query on alerts.locations  WHERE alerts.users.subscribed === TRUE
-
-    PERFORM pg_notify('alerts', alert::text);
-
+  -- Get any alert locations which are within range of the new report and issue a notification
+  FOR alert IN SELECT locations.pkey as location_key, locations.userkey, NEW.pkey as report_key, ST_DISTANCE(NEW.the_geom::geography, locations.the_geom::geography) as alerting_distance, locations.alert_log FROM alerts.locations WHERE ST_DWITHIN(NEW.the_geom::geography, locations.the_geom, 5000)
+  LOOP -- Loop locations and issue alert
+    PERFORM pg_notify('alerts', LEFT(row_to_json(alert)::text, 7999)); -- crop payload to max of 8000 bytes (UTF8 1 char = 1 byte)
  END LOOP;
  RETURN NEW;
 END;
@@ -33,29 +24,3 @@ CREATE TRIGGER watch_reports_alerts_trigger
   AFTER UPDATE ON cognicity.all_reports
   FOR EACH ROW
   EXECUTE PROCEDURE alerts.compute();
-
--- Subscribe a GRASP user to alerts service
-CREATE OR REPLACE FUNCTION alerts.register()
-  RETURNS trigger AS
-    $BODY$
-      DECLARE
-      --something
-      userkey BIGINT;
-      BEGIN
-
-      INSERT INTO alerts.users (username, network, subscribed) SELECT c.username, c.network, c.language FROM grasp.cards c WHERE c.card_id = NEW.card_id ON CONFLICT c.username DO NOTHING RETURNING pkey INTO userkey;
-
-      INSERT INTO alerts.locations (userkey, location) VALUES (userkey, NEW.the_geom);
-
-      RETURN NEW;
-    END;
-    $BODY$
-      LANGUAGE plpgsql VOLATILE
-      COST 100;
-    ALTER FUNCTION alerts.register()
-      OWNER TO postgres;
-
-CREATE TRIGGER register_grasp_users_alerts_trigger
-  BEFORE INSERT ON grasp.reports
-  FOR EACH ROW
-  EXECUTE PROCEDURE alerts.register();
--- schema/alerts/alerts.schema.sql
+++ schema/alerts/alerts.schema.sql
@@ -15,36 +15,11 @@ CREATE TABLE alerts.users (
 CREATE TABLE alerts.locations (
   pkey BIGSERIAL PRIMARY KEY,
   userkey BIGINT REFERENCES alerts.users(pkey),
+  subscribed BOOLEAN,
+  alert_log JSONB,
   the_geom GEOMETRY(Point,4326),
   UNIQUE (userkey, the_geom)
 );
 
 -- Add a GIST spatial index
 CREATE INDEX gix_alert_locations ON alerts.locations USING gist (the_geom);
-
--- Table to log alerts issued to users by location
-CREATE TABLE alerts.log (
-  pkey BIGSERIAL PRIMARY KEY,
-  location_key BIGSERIAL REFERENCES alerts.locations(pkey),
-  log_time TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  log_metadata JSON
-);
-
--- SAMPLE DATA FOR Testing
-INSERT INTO alerts.users(username, network, language, subscribed) VALUES
-('tomas', 'twitter', 'en', TRUE);
-
-INSERT INTO alerts.users(username, network, language, subscribed) VALUES
-('adi', 'twitter', 'en', TRUE);
-
-INSERT INTO alerts.locations (userkey, the_geom) VALUES
-(1, ST_GeomFromText('POINT(1 1)',4326));
-
-INSERT INTO alerts.locations (userkey, the_geom) VALUES
-(1, ST_GeomFromText('POINT(1 2)',4326));
-
-INSERT INTO alerts.locations (userkey, the_geom) VALUES
-(2, ST_GeomFromText('POINT(1 3)',4326));
-
-INSERT INTO alerts.log (location_key, log_metadata) VALUES
-(1, '{"value":"key"}');
