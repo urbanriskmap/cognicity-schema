3529f1ca8835933ce854bc22f00ab6d866a6dca5
abrahamq <abrahamquintero2323@gmail.com>
2018-09-05 13:26:19 -0400

    new release candidate for outreach

--- build/run.sh
+++ build/run.sh
@@ -69,10 +69,6 @@ if [ $SCHEMA == true ]; then
   # Load the pumps, floodgates and waterways infrastructure schema data
   psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -f $(pwd)/schema/infrastructure/infrastructure.schema.sql
 
-  echo "Load water sensor schema"
-  # Load water sensor schema
-  psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -f $(pwd)/schema/sensors/watersensor.schema.sql
-
   echo "Load sensors schema"
   # Load sensors schema
   psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -f $(pwd)/schema/sensors/sensors.schema.sql
