dbef0f5cef2d43e7489ee9bf6b3f9e711eba7c4b
Tomas <tomasholderness@gmail.com>
2018-11-02 17:05:11 +0000

    add support for port in upgrade

--- build/upgrade.sh
+++ build/upgrade.sh
@@ -4,7 +4,7 @@
 # Run this from the root directory e.g. cognicity-schema/
 
 # Define databases
-PROD=cognicity
+PROD=peta1
 NEW=new_cognicity
 
 # Set env vars
@@ -13,6 +13,7 @@ export DATA=false # Disable default data loading in schema script
 export PGHOST=localhost
 export PG_DEFAULT_DB=cognicity # Upgrade so this exists
 export PGUSER=postgres
+export PGPORT=5432
 
 # Create new database, empty schema
 source build/run.sh
@@ -22,16 +23,16 @@ source build/run.sh
 ## psql -d $PROD -h $PGHOST -U $PGUSER -f schema/reports/grasp/grasp.uuid_upgrade.sql
 
 # Copy the old data to the new database
-pg_dump -a $PROD -h $PGHOST -U $PGUSER --disable-triggers -t 'cognicity.*' -t 'detik.*' -t 'floodgauge.*' -t 'grasp.*' -t 'infrastructure.*' -t 'qlue.*' -t 'sensors.*' -t 'twitter.*' | psql -d $NEW -h $PGHOST -U $PGUSER
+pg_dump -a $PROD -h $PGHOST -U $PGUSER -p $PGPORT --disable-triggers -t 'cognicity.*' -t 'detik.*' -t 'floodgauge.*' -t 'grasp.*' -t 'infrastructure.*' -t 'qlue.*' -t 'sensors.*' -t 'twitter.*' | psql -d $NEW -h $PGHOST -U $PGUSER
 
 # Kill prod connections
-psql -d $PROD -h $PGHOST -U $PGUSER -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PROD' AND pid <> pg_backend_pid();"
+psql -d $PROD -h $PGHOST -U $PGUSER -p $PGPORT -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PROD' AND pid <> pg_backend_pid();"
 
-psql -d $NEW -h $PGHOST -U $PGUSER -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$NEW' AND pid <> pg_backend_pid();"
+psql -d $NEW -h $PGHOST -U $PGUSER -p $PGPORT -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$NEW' AND pid <> pg_backend_pid();"
 
 # Append prod with _old suffix
 OLD=$PROD'_old'
-psql -d $NEW -h $PGHOST -U $PGUSER -c "ALTER DATABASE \"$PROD\" RENAME TO \"$OLD\""
+psql -d $NEW -h $PGHOST -U $PGUSER -p $PGPORT -c "ALTER DATABASE \"$PROD\" RENAME TO \"$OLD\""
 
 # Rename new to prod
-psql -d $OLD -h $PGHOST -U $PGUSER -c "ALTER DATABASE \"$NEW\" RENAME TO \"$PROD\""
+psql -d $OLD -h $PGHOST -U $PGUSER -p $PGPORT -c "ALTER DATABASE \"$NEW\" RENAME TO \"$PROD\""
